29dc358867bf868804940c50e67cb362846d7d95
Mkdenno <dennis.kiprop@student.moringaschool.com>
2022-10-14 09:41:09 +0300

    it will add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,35 +1,35 @@
-# Rails.application.routes.draw do
-#   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-#   # Defines the root path route ("/")
-#   # root "articles#index"
-# # user routes
-#   post '/signup', to: 'users#create'
-#   get '/me', to: 'users#show'
-#   post '/login', to: 'sessions#create'
-#   delete '/logout', to: 'sessions#destroy'
+  # Defines the root path route ("/")
+  # root "articles#index"
+# user routes
+  post '/signup', to: 'users#create'
+  get '/me', to: 'users#show'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 
-# # Doctors routes
-# get '/docs', to: 'doctors#index'
+# Doctors routes
+get '/docs', to: 'doctors#index'
 
-# # Appointment routes
-# get '/appointments', to: 'appointments#index'
+# Appointment routes
+get '/appointments', to: 'appointments#index'
 
-# post '/appointments', to: 'appointments#create'
-# patch '/appointments', to: 'appointments#update'
+post '/appointments', to: 'appointments#create'
+patch '/appointments', to: 'appointments#update'
 
 
-#   resources :appointments
-#   # get '/users', to: 'users#show'
-#   # doctors login
-#   # get '/me', to: 'doctors#show'
-#   # post '/adminlogin', to: 'admin_sessions#create'
-#   # delete '/adminlogout', to: 'admin_sessions#destroy'
+  resources :appointments
+  # get '/users', to: 'users#show'
+  # doctors login
+  # get '/me', to: 'doctors#show'
+  # post '/adminlogin', to: 'admin_sessions#create'
+  # delete '/adminlogout', to: 'admin_sessions#destroy'
 
-#   # admin routes
-#   post '/signupa', to: 'admins#create'
-#   get '/mea', to: 'admins#show'
-#   post '/logina', to: 'admin_sessions#create'
-#   delete '/logouta', to: 'admin_sessions#destroy'
+  # admin routes
+  post '/signupa', to: 'admins#create'
+  get '/mea', to: 'admins#show'
+  post '/logina', to: 'admin_sessions#create'
+  delete '/logouta', to: 'admin_sessions#destroy'
 
-# end
+end
