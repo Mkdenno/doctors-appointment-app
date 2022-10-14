fd222c965d696fc38dc15bad4a0c546dee0d7d43
Mkdenno <dennis.kiprop@student.moringaschool.com>
2022-10-14 09:43:27 +0300

    it will display doctors list

--- client/src/components/DocList.js
+++ client/src/components/DocList.js
@@ -48,41 +48,41 @@ const DocList = ({
 //   };
 
 
-//   return (
-//     <div>
-//       <div className="cardContainer">
-//         <div className="card">
-//           <div className="data">
-//             <h1>
-//               <span className="name">Name</span>:{username}
-//             </h1>
-//             <p>
-//               <span className="abouts">Email</span>:{email}
-//             </p>
-//             <p>
-//               <span className="abouts">About Doctor</span>:{description}
-//             </p>
-//             <p>
-//               <span className="abouts">Location</span>:{location}
-//             </p>
-//           </div>
-//           <button
-//             className="bg-blue-600 text-gray-200  p-2 rounded  hover:bg-blue-500 hover:text-gray-100"
-//             onClick={() => {
-//               handlesubmit(username);
-//               setIsClicked(true);
-//             }}
-//           >
-//             {
+  return (
+    <div>
+      <div className="cardContainer">
+        <div className="card">
+          <div className="data">
+            <h1>
+              <span className="name">Name</span>:{username}
+            </h1>
+            <p>
+              <span className="abouts">Email</span>:{email}
+            </p>
+            <p>
+              <span className="abouts">About Doctor</span>:{description}
+            </p>
+            <p>
+              <span className="abouts">Location</span>:{location}
+            </p>
+          </div>
+          <button
+            className="bg-blue-600 text-gray-200  p-2 rounded  hover:bg-blue-500 hover:text-gray-100"
+            onClick={() => {
+              handlesubmit(username);
+              setIsClicked(true);
+            }}
+          >
+            {
 
-//              appointmentData.status == true? "booked"  : appointmentStatus
-//             }
-//           </button>
-//             <p>{ !isClicked? "": "pending" }</p>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
+             appointmentData.status == true? "booked"  : appointmentStatus
+            }
+          </button>
+            <p>{ !isClicked? "": "pending" }</p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-// export default DocList;
+export default DocList;
