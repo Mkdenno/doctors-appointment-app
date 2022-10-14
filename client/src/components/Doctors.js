29152529f44a680cea1643e11c65a63b95dd5bc5
Mkdenno <dennis.kiprop@student.moringaschool.com>
2022-10-14 09:45:28 +0300

    it will add fetct from doctors from database

--- client/src/components/Doctors.js
+++ client/src/components/Doctors.js
@@ -2,13 +2,13 @@ import React, { useEffect, useState } from 'react'
 import DocList from './DocList';
 
 const Doctors = ({user}) => {
-    // const [doctor, setDoctor]=useState([]);
-    // useEffect(()=>{
-    //     fetch("/docs")
-    //     .then((r)=>r.json())
-    //     .then((data)=>setDoctor(data))
+    const [doctor, setDoctor]=useState([]);
+    useEffect(()=>{
+        fetch("/docs")
+        .then((r)=>r.json())
+        .then((data)=>setDoctor(data))
 
-    // },[])
+    },[])
     // const displayDoc=doctor.map((doc)=>(
     //  <DocList
     //  key={doc.id}
